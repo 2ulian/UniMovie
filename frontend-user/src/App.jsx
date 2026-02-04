3f524137251db4d1368b579054643cc0fa0e4121
Julian <rayconstantyjulian@gmail.com>
2026-02-04 15:06:32 +0100

    update App, Navbar and styles

--- frontend-user/src/App.jsx
+++ frontend-user/src/App.jsx
@@ -1,18 +1,7 @@
-import logo from './assets/react.svg';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <div className="text-center">
-        <div className="flex items-center justify-center mb-4">
-          <img src={logo} alt="Logo" className="h-12 w-12 mr-4" />
-          <h1 className="text-6xl font-bold text-primary">Hello Netphlix</h1>
-        </div>
-        <p className="text-xl text-gray-400">
-          Frontend User - React + Tailwind 4.1
-        </p>
-      </div>
-    </div>
-  );
+  return <Home />;
 }
+
 export default App;
